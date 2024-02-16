31ff9ffd8e06073046ee4586bb60e79f77b50660
Mike0001-droid <majorov-2004@list.ru>
2024-02-16 10:07:03 +0500

    Вернули функцию

--- send_msg/whatsapp/models.py
+++ send_msg/whatsapp/models.py
@@ -4,6 +4,5 @@ class BookOfPhone (models.Model):
     name = models.CharField('Имя человека', max_length=20)
     phone = models.IntegerField('Номер телефона')
 
-    """ def __str__(self):
+    def __str__(self):
         return f"{self.pk}) {self.name} Номер: {self.phone}"
- """
